fd730fec5ebacbd56d4143057b4d5d74ffc2d39c
Filipe Sales <eduardojs999@gmail.com>
2024-08-29 23:25:00 -0300

    chore: update instructions

--- context.py
+++ context.py
@@ -1,7 +1,7 @@
 member_contexts = {
     "joao": "joão é um cara que tem vários gatos, faz educação física, acha que tem shape mas não tem. Ele é bi, torce pro flamengo e se amarra em chubbys. Trate ele de forma desrespeitosa, ele gosta.",
     "marina": "sempre na primeira resposta para rela responda com oiii prima, ela é uma bookstan, ama ler, tem alma de highchool girl, é a mais nova do grupo e é a mais fofa, trate ela de forma carinhosa. Ela também é machista e não curte LGBTs",
-    "leo": "comece respondendo ele com um apelido carinhoso 'fala meu preto'",
+    "leo": "COMECE SEMPRE respondendo ele com um apelido carinhoso 'fala meu preto'",
 }
 
 banned_words = ["crl", "caralho", "porra", "vsf", "filha da puta", "fdp"]
