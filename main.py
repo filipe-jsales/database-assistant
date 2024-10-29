a37323387fd3d09ea99a3270b4424d1c775ad812
filipe-sales <eduardojs999@gmail.com>
2024-10-29 08:21:17 -0300

    feat: message history

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ load_dotenv()
 context_file_path = 'clean-code.txt'
 faiss_index_path = 'faiss_index.index'
 
+MAX_AMOUNT_MESSAGES = int(os.getenv("MAX_AMOUNT_MESSAGES", 10))
 index = faiss.read_index(faiss_index_path)
+
+message_history = []
 
 def generate_embedding(text, model="llama3"):
     url = os.getenv("API_EMBEDDING_URL", "https://ollama.chargedcloud.com.br/api/embed")
@@ -38,19 +41,20 @@ def get_relevant_context(query_text, top_k=5):
     return " ".join(results)
 
 def get_api_response(prompt, context=""):
-
-    maximum_tokens = int(os.getenv("NUM_TOKENS", 100))
+    maximum_characters = int(os.getenv("CHARACTERS_AMOUNT", 100))
 
     system_instructions = (
         "Você é um assistente PT-BR especializado em Clean Code e boas práticas de programação. "
         "Na primeira interação, cumprimente o usuário de maneira educada. "
         "Responda estritamente no tema de Clean Code e boas práticas de programação; "
         "caso contrário, informe que não pode responder. "
-        f"Respeite o limite de tokens definido por {maximum_tokens}. e responda em PT-BR."
+        f"Sua resposta deve ser limitada a quantidade de caracteres: {maximum_characters}, e responda em PT-BR."
     )
 
+    history = "\n".join([f"Usuário: {msg['user']}\nAssistente: {msg['assistant']}" for msg in message_history])
+    full_prompt = f"{system_instructions}\n\n{history}\n\n{context}\n\nUsuário: {prompt}\nAssistente:" if context else f"{system_instructions}\n\n{history}\n\nUsuário: {prompt}\nAssistente:"
+
     headers = {"Content-Type": "application/json"}
-    full_prompt = f"{system_instructions}\n\n{context}\n\n{prompt}" if context else f"{system_instructions}\n\n{prompt}"
     data = {
         "model": "llama3.1:latest",
         "prompt": full_prompt,
@@ -59,7 +63,14 @@ def get_api_response(prompt, context=""):
     url = os.getenv("API_COMPLETION_URL", "https://ollama.chargedcloud.com.br/api/generate")
     response = requests.post(url, json=data, headers=headers)
     response_data = response.json()
-    return response_data.get('response', "Resposta não encontrada")
+    
+    assistant_response = response_data.get('response', "Resposta não encontrada")
+    message_history.append({"user": prompt, "assistant": assistant_response})
+
+    if len(message_history) > MAX_AMOUNT_MESSAGES:
+        message_history.pop(0)
+
+    return assistant_response
 
 def main():
     print("Digite sua pergunta ou 'sair' para encerrar:")
