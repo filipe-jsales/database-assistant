90a6b542d4800b5c90891f4a73f657e7f4ec8661
filipe-sales <eduardojs999@gmail.com>
2024-10-29 08:10:34 -0300

    feat: instructions

--- generate-embeddings.py
+++ generate-embeddings.py
@@ -4,14 +4,11 @@ import faiss
 import numpy as np
 from dotenv import load_dotenv
 
-# Configuração inicial e carga de variáveis de ambiente
 load_dotenv()
 embedding_dimension = 4096
 index = faiss.IndexFlatL2(embedding_dimension)
 context_file_path = 'clean-code.txt'
-faiss_index_path = 'faiss_index.index'  # Caminho para salvar o índice FAISS
-
-# Função para gerar embeddings usando a API externa
+faiss_index_path = 'faiss_index.index'
 def generate_embeddings(texts, model="llama3.1:latest"):
     url = os.getenv("API_EMBEDDING_URL", "https://ollama.chargedcloud.com.br/api/embed")
     data = {
@@ -23,12 +20,10 @@ def generate_embeddings(texts, model="llama3.1:latest"):
     response.raise_for_status()
     return response.json().get("embeddings", [])
 
-# Função para salvar o índice FAISS em disco
 def save_index():
     faiss.write_index(index, faiss_index_path)
     print("Índice FAISS salvo em", faiss_index_path)
 
-# Função para carregar o índice FAISS do disco
 def load_index():
     global index
     if os.path.exists(faiss_index_path):
@@ -37,23 +32,19 @@ def load_index():
     else:
         print("Nenhum índice FAISS encontrado. Criando um novo.")
 
-# Função para armazenar embeddings no índice FAISS
 def store_embeddings_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         texts = [line.strip() for line in file if line.strip()]
 
-    # Gerar embeddings para cada linha do arquivo
     embeddings = generate_embeddings(texts)
 
-    # Converter para um array numpy e adicionar ao índice FAISS
     embeddings_np = np.array(embeddings).astype('float32')
     index.add(embeddings_np)
 
     print(f"{len(embeddings)} embeddings foram adicionados ao índice FAISS.")
-    save_index()  # Salva o índice FAISS em disco
+    save_index()
 
-# Gera e armazena embeddings do arquivo de contexto, se ainda não estiverem salvos
 if not os.path.exists(faiss_index_path):
     store_embeddings_from_file(context_file_path)
 else:
-    load_index()  # Carrega o índice FAISS se já estiver salvo
+    load_index()
--- main.py
+++ main.py
@@ -4,15 +4,12 @@ import numpy as np
 import faiss
 from dotenv import load_dotenv
 
-# Configuração inicial e carga de variáveis de ambiente
 load_dotenv()
 context_file_path = 'clean-code.txt'
-faiss_index_path = 'faiss_index.index'  # Caminho para o índice FAISS
+faiss_index_path = 'faiss_index.index'
 
-# Carrega o índice FAISS salvo
 index = faiss.read_index(faiss_index_path)
 
-# Função para gerar embeddings usando a API externa
 def generate_embedding(text, model="llama3"):
     url = os.getenv("API_EMBEDDING_URL", "https://ollama.chargedcloud.com.br/api/embed")
     data = {
@@ -24,7 +21,6 @@ def generate_embedding(text, model="llama3"):
     response.raise_for_status()
     return response.json().get("embeddings", [])[0]
 
-# Função para listar os documentos armazenados no arquivo de contexto
 def list_documents():
     if os.path.exists(context_file_path):
         with open(context_file_path, 'r') as file:
@@ -32,7 +28,6 @@ def list_documents():
     else:
         return []
 
-# Função para buscar o contexto relevante usando FAISS
 def get_relevant_context(query_text, top_k=5):
     query_embedding = generate_embedding(query_text)
     query_np = np.array([query_embedding]).astype('float32')
@@ -40,23 +35,32 @@ def get_relevant_context(query_text, top_k=5):
     distances, indices = index.search(query_np, k=top_k)
     all_texts = list_documents()
     results = [all_texts[i] for i in indices[0] if i < len(all_texts)]
-    return " ".join(results)  # Combina os resultados em um único contexto
+    return " ".join(results)
 
-# Função para fazer a chamada à API com o contexto
 def get_api_response(prompt, context=""):
+
+    maximum_tokens = int(os.getenv("NUM_TOKENS", 100))
+
+    system_instructions = (
+        "Você é um assistente PT-BR especializado em Clean Code e boas práticas de programação. "
+        "Na primeira interação, cumprimente o usuário de maneira educada. "
+        "Responda estritamente no tema de Clean Code e boas práticas de programação; "
+        "caso contrário, informe que não pode responder. "
+        f"Respeite o limite de tokens definido por {maximum_tokens}. e responda em PT-BR."
+    )
+
     headers = {"Content-Type": "application/json"}
-    full_prompt = f"{context}\n\n{prompt}" if context else prompt
+    full_prompt = f"{system_instructions}\n\n{context}\n\n{prompt}" if context else f"{system_instructions}\n\n{prompt}"
     data = {
         "model": "llama3.1:latest",
         "prompt": full_prompt,
         "stream": False
     }
-    
-    response = requests.post("https://ollama.chargedcloud.com.br/api/generate", json=data, headers=headers)
+    url = os.getenv("API_COMPLETION_URL", "https://ollama.chargedcloud.com.br/api/generate")
+    response = requests.post(url, json=data, headers=headers)
     response_data = response.json()
     return response_data.get('response', "Resposta não encontrada")
 
-# Função principal para perguntas e respostas com contexto FAISS
 def main():
     print("Digite sua pergunta ou 'sair' para encerrar:")
     
@@ -68,7 +72,6 @@ def main():
             break
         
         try:
-            # Obtém o contexto relevante do índice FAISS
             context = get_relevant_context(user_input)
             response = get_api_response(user_input, context)
             print("Resposta da API:", response)
